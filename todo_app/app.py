98a1d6acde0bc4cb48c888e11ab59cb4f3c5f4ab
Tom Bell <tom.bell@softwire.com>
2020-04-06 18:14:16 +0200

    Add functionality to mark items as complete

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 from todo_app.flask_config import Config
 from todo_app.data import session_items as session
@@ -12,11 +12,18 @@ def index():
     items = session.get_items()
     return render_template('index.html', items = items)
 
+
 @app.route('/items/new', methods=['POST'])
 def add_item():
     title = request.form['title']
     session.add_item(title)
-    return redirect(url_for('index')) 
+    return redirect(url_for('index'))
+
+
+@app.route('/items/<id>/complete')
+def complete_item(id):
+    session.complete_item(id)
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
